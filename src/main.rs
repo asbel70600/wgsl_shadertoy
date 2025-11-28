467c8ea51931033a236fbe3e7eee68eb7f8f06cc
asbel <asbel70600@gmail.com>
2025-11-27 20:37:44 -0500

    feat: add basic windowing event loop

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+#![warn(clippy::pedantic)]
+#![allow(clippy::separated_literal_suffix, reason = "WHTF")]
+#![allow(
+    clippy::question_mark,
+    reason = "for a cleaner syntax errors are bubbled up"
+)]
+#![allow(
+    clippy::question_mark_used,
+    reason = "for a cleaner syntax errors are bubbled up"
+)]
+#![allow(
+    clippy::blanket_clippy_restriction_lints,
+    reason = "i want to do it as it says"
+)]
+
+use core::error::Error;
+use tracing::{Level, span};
+use winit::event_loop::EventLoop;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+    let span = span!(Level::WARN, "MAIN_THREAD").entered();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+
+    span.exit();
+    Ok(())
+}
